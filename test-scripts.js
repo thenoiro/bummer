23df9e8577141939df77181e2276ad22f3f24970
Bohdan Chernovol <noiro.bogdan@gmail.com>
2019-12-10 23:02:58 +0200

    BIRTHDAY COMMIT =) . More tests. Fix styles according to the airbnb rules (js only).

---
+++ test-scripts.js
@@ -0,0 +1,80 @@
+const { log, logError } = require('./porter-logger');
+
+const FAIL = 'FAIL';
+const SUCCESS = 'SUCCESS';
+
+let testCounter = 1;
+
+const logTestResults = (testMsg, request, expected, result) => {
+  const delimiter = '-------------------------------------\n';
+  log(...[
+    delimiter,
+    `TEST ${testCounter += 1}: ${testMsg}\n`,
+    `Request: ${request}\n`,
+    // `Expected: ${expected}\n`,
+    '...\n',
+    `Result: ${result()}\n`,
+  ]);
+};
+
+const testScripts = [
+  /** Test porter.get(object, path) */
+  (porter, target) => {
+    const testMessage = 'Porter.get: Should return the value by simple path.';
+    const expected = target.games['Half-Life'];
+    const requests = [
+      'games.Half-Life',
+      'games[Half-Life]',
+      "games['Half-Life']",
+    ];
+    requests.forEach((path) => {
+      const requestString = `porter.get(testSubject, '${path}')`;
+      const callback = () => {
+        let result = FAIL;
+
+        try {
+          const value = porter.get(target, path);
+
+          if (value === expected) {
+            result = SUCCESS;
+          }
+        } catch (ex) {
+          logError(ex);
+        }
+        return result;
+      };
+      logTestResults(testMessage, requestString, expected, callback);
+    });
+  },
+
+  /** Test porter(object).get(path) */
+  (porter, target) => {
+    const testMessage = 'porterInstance.get. Should return the value by simple path.';
+    const expected = target.games['Half-Life'];
+    const requests = [
+      'games.Half-Life',
+      'games[Half-Life]',
+      "games['Half-Life']",
+    ];
+    requests.forEach((path) => {
+      const requestString = `porter(testSubject).get('${path}')`;
+      const callback = () => {
+        let result = FAIL;
+
+        try {
+          const value = porter(target).get(path);
+
+          if (value === expected) {
+            result = SUCCESS;
+          }
+        } catch (ex) {
+          logError(ex);
+        }
+        return result;
+      };
+      logTestResults(testMessage, requestString, expected, callback);
+    });
+  },
+];
+
+export default { testScripts }
