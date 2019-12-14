35de5c549d92bb3100ec10481bb98fe5b817ab5d
Bohdan Chernovol <noiro.bogdan@gmail.com>
2019-12-14 17:59:32 +0200

    More tests, and code updates - JSDoc comments - Porter.set tests added - Display summary 'Total'

--- test-scripts.js
+++ test-scripts.js
@@ -1,28 +1,62 @@
+/** @typedef {('SUCCESS'|'FAIL')} TestResult */
+/** @typedef {*} executorResults */
+
+/**
+ * @callback executorCallback
+ * @returns {*} Any value which will be passed to the moderator callback later.
+ */
+
+/**
+ * @callback moderatorCallback
+ * @param {executorResults} value Any value returned by executor callback.
+ * @returns {boolean} Is test completed successfully or not.
+ */
+
 const { log, logError } = require('./porter-logger');
 
+let testCounter = 0;
+const tests = [];
 const FAIL = 'FAIL';
 const SUCCESS = 'SUCCESS';
-let testCounter = 0;
+const has = (object, property) => Object.prototype.hasOwnProperty.call(object, property);
 
 const summary = {
   [FAIL]: 0,
   [SUCCESS]: 0,
 };
+const requests = [
+  'games.Half-Life',
+  'games[Half-Life]',
+  "games['Half-Life']",
+];
 
+/**
+ * Log test meta and results
+ * @param {string} testMsg Test message
+ * @param {string} request String representation of tested feature
+ * @param {string} result Test results (Success or Fail)
+ */
 const logResults = (testMsg, request, result) => {
   log(...[
     `TEST ${testCounter += 1}: ${testMsg}\n`,
     `Request: ${request}\n`,
-    `... ${result()}\n`,
+    `... ${result}\n`,
   ]);
 };
-const testLauncher = (executor, expected) => {
+
+/**
+ * Launches the test, and returns the result
+ * @param {executorCallback} executor  Tested feature.
+ * @param {moderatorCallback} moderator Results inspector.
+ * @returns {TestResult}
+ */
+const testLauncher = (executor, moderator) => {
   let result = FAIL;
 
   try {
     const value = executor();
 
-    if (value === expected) {
+    if (moderator(value)) {
       result = SUCCESS;
     }
   } catch (ex) {
@@ -32,22 +66,16 @@ const testLauncher = (executor, expected) => {
   return result;
 };
 
-const tests = [];
-const requests = [
-  'games.Half-Life',
-  'games[Half-Life]',
-  "games['Half-Life']",
-];
-
 /** Test porter.get(object, path) */
 requests.forEach((path) => {
   tests.push((porter, target) => {
-    const executor = () => porter.get(target, path);
-    const callback = () => testLauncher(executor, target.games['Half-Life']);
     logResults(
       'Porter.get: Should return the value by simple path.',
       `porter.get(testSubject, '${path}')`,
-      callback,
+      testLauncher(
+        () => porter.get(target, path),
+        (v) => v === target.games['Half-Life'],
+      ),
     );
   });
 });
@@ -55,12 +83,81 @@ requests.forEach((path) => {
 /** Test porter(object).get(path) */
 requests.forEach((path) => {
   tests.push((porter, target) => {
-    const executor = () => porter(target).get(path);
-    const callback = () => testLauncher(executor, target.games['Half-Life']);
     logResults(
       'Porter.get: Should return the value by simple path.',
       `porter.get(testSubject, '${path}')`,
-      callback,
+      testLauncher(
+        () => porter(target).get(path),
+        (v) => v === target.games['Half-Life'],
+      ),
+    );
+  });
+});
+
+/** Test porter.set(object, path, value) */
+requests.forEach((path) => {
+  tests.push((porter/* , target */) => {
+    const target = {};
+    const settedValue = Symbol('Value');
+
+    logResults(
+      'Porter.set: Should set the value by non-existing path, and return the <true> value.',
+      `porter.set({}, '${path}')`,
+      testLauncher(
+        () => porter.set(target, path),
+        (v) => v === true && target.games['Half-Life'] === settedValue,
+      ),
+    );
+  });
+});
+
+/** Test porter(object).set(path, value) */
+requests.forEach((path) => {
+  tests.push((porter/* , target */) => {
+    const target = {};
+    const settedValue = Symbol('Value');
+
+    logResults(
+      'Porter.set: Should set the value by non-existing path, and return the <true> value.',
+      `porter({}).set('${path}')`,
+      testLauncher(
+        () => porter(target).set(path),
+        (v) => v === true && target.games['Half-Life'] === settedValue,
+      ),
+    );
+  });
+});
+
+/** Test porter.set(object, path, value, false) */
+requests.forEach((path) => {
+  tests.push((porter/* , target */) => {
+    const target = {};
+    const settedValue = Symbol('Value');
+
+    logResults(
+      'Porter.set: Should try to set the value by non-existing path, and return the <false> value.',
+      `porter.set({}, '${path}')`,
+      testLauncher(
+        () => porter.set(target, path, settedValue),
+        (v) => v === false && !has(target, 'games'),
+      ),
+    );
+  });
+});
+
+/** Test porter(object).set(path, value, false) */
+requests.forEach((path) => {
+  tests.push((porter/* , target */) => {
+    const target = {};
+    const settedValue = Symbol('Value');
+
+    logResults(
+      'Porter.set: Should try to set the value by non-existing path, and return the <false> value.',
+      `porter({}).set('${path}')`,
+      testLauncher(
+        () => porter(target).set(path, settedValue),
+        (v) => v === false && !has(target, 'games'),
+      ),
     );
   });
 });
@@ -69,6 +166,7 @@ requests.forEach((path) => {
 tests.push((/* porter, target */) => {
   log(
     'SUMMARY:\n',
+    `> Total: ${testCounter}\n`,
     `> Success: ${summary[SUCCESS]}\n`,
     `> Fails: ${summary[FAIL]}`,
   );
